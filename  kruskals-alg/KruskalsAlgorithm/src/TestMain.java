1872a4391ac897783423d522b10a41b84a35bc03
dyk003@uw.edu <dyk003@uw.edu@aa04321d-62e8-176e-da4c-d5c2f727d064>
2014-11-28 20:10:43 +0000

    Here's the TestMain

---
+++  kruskals-alg/KruskalsAlgorithm/src/TestMain.java
@@ -0,0 +1,29 @@
+
+
+import java.util.Iterator;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		SimpleGraph G = new SimpleGraph();
+		GraphInput.LoadSimpleGraph(G);  
+		Vertex v;
+		Edge e;
+		Iterator i;
+		System.out.println("Iterating through vertices...");
+        for (i= G.vertices(); i.hasNext(); ) {
+            v = (Vertex) i.next();
+            System.out.println("found vertex " + v.getName());
+        }
+        System.out.println("Iterating through adjacency lists...");
+        for (i= G.vertices(); i.hasNext(); ) {
+            v = (Vertex) i.next();
+            System.out.println("Vertex "+v.getName());
+            Iterator j; 
+            for (j = G.incidentEdges(v); j.hasNext();) {
+                e = (Edge) j.next();
+                System.out.println("  found edge " + e.getData());
+            }
+        }
+	} 
+}
